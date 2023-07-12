20d97dedd5fc69722d53d58a607c3f434d6777ed
NevesTi <ceelso.7@gmail.com>
2023-07-12 17:07:31 -0300

    andando

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
-import HomePage from './src/pages/home_pages';
+import { HomePage } from './src/pages/home_pages'; 
 
 export default function App() {
   return (
     <View style={styles.container}> 
       <HomePage/>
-    </View>
-  );
+    </View> );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
+  
 });
--- src/pages/home_pages.tsx
+++ src/pages/home_pages.tsx
@@ -1,46 +1,184 @@
-import React, { useState, useRef } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+import { View, StyleSheet, Text,Image,TouchableOpacity } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
+import React, { useRef, useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
+
+
+export function HomePage() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [cameraPosition, setCameraPosition] = useState(null);
+  const [origin, setOrigin] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+
+  const handleMapPress = (event) => {
+    const { coordinate } = event.nativeEvent;
+    setCameraPosition(coordinate);
+  };
+
+  const handleCameraRegionChange = (region) => {
+    const { latitude, longitude } = region;
+    setCameraPosition({ latitude, longitude });
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <MapView style={{ width: '100%', height: '100%' }}
+        initialRegion={{
+          latitude: -22.1214571,
+          longitude: -43.2061884,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+
+        }}
+      >
+
+        {cameraPosition && (
+          <Marker coordinate={cameraPosition} title="Camera Position" />
+        )}
+
+        <Marker
+          coordinate={{ latitude: -22.1214571, longitude: -43.2061884 }}
+          title="Praça "
+          description="Praça são Sebastião"
+        />
+
+
+
+      </MapView>
+
+      <Camera
+        style={styles.camera}
+        ref={(ref) => setCameraRef(ref)}
+        type={Camera.Constants.Type.back}
+        autoFocus={Camera.Constants.AutoFocus.on}
+      />
+      <View style={styles.container_button}>
+      <TouchableOpacity style={styles.buttonCamera}>
+        <Text>celso neves</Text>
+      </TouchableOpacity>
+      </View>
+      
+    </View>
+
+    
+  )
+}
+
+const styles = StyleSheet.create({
+
+  map: {
+    flex: 1
+  },
+
+  camera: {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    width: 70,
+    height: 70,
+    borderRadius: 8,
+    marginBottom: 100
+  },
+  buttonCamera:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    width:100,
+    height:100,
+    backgroundColor:'#6676f1',
+  },
+  container_button:{
+    width:100,
+    height:100,
+    backgroundColor:'#FF0000',
+    
+    
+
+  }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { useRef, useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Camera } from 'expo-camera';
 
 export default function HomePage() {
-  const [cameraActive, setCameraActive] = useState(false);
-  const mapRef = useRef(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [cameraPosition, setCameraPosition] = useState(null);
+    const [origin, setOrigin] = useState(null);
 
-  const toggleCamera = () => {
-    setCameraActive(!cameraActive);
-  };
+
+  useEffect(() => {
+    (async  () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
 
   const handleMapPress = (event) => {
-    if (cameraActive) {
-      const { coordinate } = event.nativeEvent;
-      // Fazer algo com as coordenadas da câmera, como enviar para um servidor, etc.
-      toggleCamera();
-    }
+    const { coordinate } = event.nativeEvent;
+    setCameraPosition(coordinate);
   };
+
+  const handleCameraRegionChange = (region) => {
+    const { latitude, longitude } = region;
+    setCameraPosition({ latitude, longitude });
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
       <MapView
-        ref={mapRef}
         style={styles.map}
         onPress={handleMapPress}
+        onRegionChange={handleCameraRegionChange}
       >
-        {/* Marcador para representar a posição da câmera */}
-        {cameraActive && (
-          <Marker coordinate={/* coordenadas da câmera */} />
+        {cameraPosition && (
+          <Marker coordinate={cameraPosition} title="Camera Position" />
         )}
       </MapView>
-
-      {/* Botão para alternar a câmera */}
-      <TouchableOpacity style={styles.button} onPress={toggleCamera}>
-        <Text style={styles.buttonText}>{cameraActive ? 'Fechar Câmera' : 'Abrir Câmera'}</Text>
-      </TouchableOpacity>
-
-      {/* Renderizar a câmera se estiver ativa */}
-      {cameraActive && (
-        <Camera style={styles.camera} />
-      )}
+      <Camera
+        style={styles.camera}
+        ref={(ref) => setCameraRef(ref)}
+        type={Camera.Constants.Type.back}
+        autoFocus={Camera.Constants.AutoFocus.on}
+      />
     </View>
   );
 }
@@ -52,23 +190,13 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
-  button: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    borderRadius: 8,
-  },
-  buttonText: {
-    color: '#fff',
-  },
   camera: {
     position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    bottom: 16,
+    right: 16,
+    width: 70,
+    height: 70,
+    borderRadius: 8,
+    marginBottom:100
   },
-});
+});*/

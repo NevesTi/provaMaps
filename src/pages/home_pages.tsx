a0621273d473b4cd68d16ce400cd3fdc84e1000f
NevesTi <ceelso.7@gmail.com>
2023-07-12 21:46:34 -0300

    feitosenai

--- src/pages/home_pages.tsx
+++ src/pages/home_pages.tsx
@@ -1,22 +1,42 @@
 
-import { View, StyleSheet, Text,Image,TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import React, { useRef, useState, useEffect } from 'react';
-import { Camera } from 'expo-camera';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibray from 'expo-media-library';
+import { Image } from 'expo-image';
+
+
+
 
 
 export function HomePage() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [cameraRef, setCameraRef] = useState(null);
+  const [Permission, setPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
   const [cameraPosition, setCameraPosition] = useState(null);
   const [origin, setOrigin] = useState(null);
+  const [image, setImage] = useState(null);
+
+
+
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setPermission(cameraStatus.status === 'granted');
+      await MediaLibray.requestPermissionsAsync();
     })();
   }, []);
+
+  async function takePicture() {
+    if (camera) {
+      // tirar uma foto
+      const { uri } = await camera.takePictureAsync();
+      console.log(uri);
+      setImage(uri);
+      await MediaLibray.saveToLibraryAsync(uri);
+    }
+  }
 
 
   const handleMapPress = (event) => {
@@ -29,10 +49,10 @@ export function HomePage() {
     setCameraPosition({ latitude, longitude });
   };
 
-  if (hasPermission === null) {
+  if (Permission === null) {
     return <View />;
   }
-  if (hasPermission === false) {
+  if (Permission === false) {
     return <Text>No access to camera</Text>;
   }
 
@@ -59,25 +79,31 @@ export function HomePage() {
           description="Praça são Sebastião"
         />
 
-
-
       </MapView>
 
       <Camera
-        style={styles.camera}
-        ref={(ref) => setCameraRef(ref)}
-        type={Camera.Constants.Type.back}
-        autoFocus={Camera.Constants.AutoFocus.on}
+        ref={(minhaCamera) => setCamera(minhaCamera)}
+        style={styles.styleCamera}
+        type={CameraType.back}
+        ratio={'1:1'}
       />
+
+      <Image
+        style={styles.image}
+        source={image}
+        contentFit="cover"
+        transition={1000} />
+
+
       <View style={styles.container_button}>
-      <TouchableOpacity style={styles.buttonCamera}>
-        <Text>celso neves</Text>
-      </TouchableOpacity>
+        <TouchableOpacity 
+        style={styles.buttonCamera} onPress={() => { takePicture() }}>
+
+          <Text>click</Text>
+        </TouchableOpacity>
       </View>
-      
     </View>
 
-    
   )
 }
 
@@ -94,23 +120,45 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     borderRadius: 8,
-    marginBottom: 100
+    marginBottom: 100,
+
+    
   },
-  buttonCamera:{
+  buttonCamera: {
     justifyContent: 'center',
     alignItems: 'center',
-    width:100,
-    height:100,
-    backgroundColor:'#6676f1',
-  },
-  container_button:{
-    width:100,
-    height:100,
-    backgroundColor:'#FF0000',
-    
-    
+    width: 100,
+    height: 100,
+    backgroundColor: '#6676f1',
+    borderRadius: 100,
+    marginTop: 300
 
+  },
+  container_button: {
+    position: 'absolute',
+    width: 100,
+    height: 100,
+  
+  },
+
+  imageCamera: {
+    width: 50,
+    height: 50,
+
+  },
+  styleCamera: {
+    aspectRatio: 1,
+    flex: 1
+  },
+
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   }
+
+
+
 
 })
 
